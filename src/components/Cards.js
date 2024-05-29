7f356a636d3e8b7ced594489963388ec9b8dea08
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2024-05-29 23:16:56 +0530

    Added All the Require Components

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import Card from './Card'
 
-const Cards = ({courses}) => {
+const Cards = (props) => {
+  let courses = props.courses;
+
+  function getCourses() {
+    let allCourses = [];
+    // Sara Object ko All Courses wala array ke ander store kar diya kyuki map() lagane ko easy hoga 
+    
+    Object.values(courses).forEach(array => {
+      array.forEach(courseData => {
+        allCourses.push(courseData);
+      })
+    })
+  }
   return (
     <div>
-      {
-        courses.map((data)=>{
-          <Card/>
-        })
-      }
+      
     </div>
   )
 }
